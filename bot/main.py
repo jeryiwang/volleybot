7a21be09e37009186b02542ba1293a01e69fd3e4
jeryiwang <wangjerry91@gmail.com>
2025-08-11 09:29:24 -0400

    31 Minute Intervals
    15 minute intervals ran into cloudflare block

--- bot/main.py
+++ bot/main.py
@@ -50,7 +50,7 @@ def keepalive():
         return "Still kickin' (barely)", 200
 
 # === Post Roster Task ===
-@tasks.loop(minutes=15)
+@tasks.loop(minutes=31)
 async def post_roster():
     logger.info("✅ post_roster() task started.")
     try:
